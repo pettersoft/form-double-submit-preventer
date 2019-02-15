6290569184db74f51880f01afd75e145844808f9
Petter <pettersoft@gmail.com>
2019-02-15 08:06:54 +0100

    Update submit-disabler.js

--- submit-disabler.js
+++ submit-disabler.js
@@ -8,16 +8,30 @@
 	});
 */
 (function ($){
-	$.fn.disableDoubleSubmit = function(){
-		
-		return this.each(function(){	
+	$.fn.disableDoubleSubmit = function(options){
+
+        var settings = $.extend({
+            disableLinks: false
+        }, options);
+
+        return this.each(function () {	
 			$(this).on("submit", function(evt){
 				evt.preventDefault();
-			
+
+                console.log("method hit");
+
 				$(this).find("input[type=submit]").attr("disabled", "disabled");
-				$(this).find("button[type=submit]").attr("disabled", "disabled");
-							
-				this.submit();
+                $(this).find("button[type=submit]").attr("disabled", "disabled");
+
+                if (settings.disableLinks === true) {
+                    $(this).find("a").attr("disabled", "disabled");
+                    $(this).find("a").attr("href", "javascript:void(0);");
+                }
+
+                console.log(this);
+
+                // this.submit() will not work if any element is named/id:d submit
+                HTMLFormElement.prototype.submit.call(this);
 			});
 		});
 		
